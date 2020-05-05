b755a84de1581cba56b5eb08b59d54e79ec5af60
Fernando Tralci <tralci@tralCI-mac.local>
2020-05-05 12:54:30 -0300

    change color bg

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
-		background-color: white;
+		background-color: red;
 		text-align: center;
 		padding: 50px;
 		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
